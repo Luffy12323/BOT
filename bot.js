f609ebf03503b7dc52c17a977b0ae4e5acca9e14
Osama2001 <40809896+Osama2001@users.noreply.github.com>
2018-07-20 12:35:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1242,7 +1242,7 @@ client.on('message', message => {
 		
 		role.delete()
 	}
-		
+		});
 		
 			message.channel.sendMessage({embed: new Discord.RichEmbed()
      .setColor('RED').setAuthor(`${message.author.username}'`, message.author.avatarURL).setDescription('``جاري حذف على الالوان |✅``')});
