c1166bc6b683c9f533f6c2d3d615aa5435f32f5a
Osama2001 <40809896+Osama2001@users.noreply.github.com>
2018-07-27 01:18:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8210,82 +8210,140 @@ if (message.content.startsWith("-ping")) {
 
 
 
-
 client.on('message', message => {
-	var prefix = "-"
-  if (message.author.x5bz) return;
-  if (!message.content.startsWith(prefix)) return;
+    if (message.content.startsWith("-kick")) {
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
 
-  let args = message.content.split(" ").slice(1);
+let kickeduser = message.mentions.users.first();
+if(!message.channel.guild) return
+if(!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send("**You Dont Have Permission 'KICK_MEMBERS' ليس لديك صلاحية**")
+if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.channel.send("**i Dont Have Permission 'KICK_MEMBERS' ليس لدي صلاحية**")
+if(!kickeduser) return message.channel.send("**i Can Not Find This User | لم اجد شخص بهاد الاسم**")
+if (!message.guild.member(kickeduser).kickable) return message.reply("**I Can't Kick This Person | لا يمكنني طرد هدا الشخص**");
+let kickreason = message.content.split(" ").slice(2).join(" ");
+if(!kickreason) return message.channel.send("Please Type Reason... | رجاءا ضع سبب")
 
-  if (command == "ban") {
-               if(!message.channel.guild) return message.reply('** This command only for servers**');
+
+var DK= new Discord.RichEmbed()
+   .setFooter("GOLD BOT", client.user.avatarURL)
+   .setColor('GOLD')
+   .setThumbnail(message.author.avatarURL)
+   .setTitle("KICKED !")
+   .addField("Kicked User:", kickeduser)
+   .addField("Kicked By:",  message.author.username)
+   .addField("Reason:", kickreason)
+
+
+   message.channel.send(DK);
+
+
+       message.guild.member(kickeduser).kick(kickreason);
+
+
+
+     let logroom = message.guild.channels.find("name", "log")
+     if(!logroom) return;
+
+     var log= new Discord.RichEmbed() 
+     
+     .setColor("RED")
+     .setThumbnail(message.author.avatarURL)
+     .addField("Kicked Member Name", kickeduser,false)
+     .addField("Kicked By", message.author.username,false)
+     .addField("Kicked In:", message.channel,false)
+     .addField("Reason:", kickreason,false)
+     .setFooter("GOLD BOT", client.user.avatarURL)
+     
+       logroom.send(log);
+
+
+       var DM= new Discord.RichEmbed() 
+     
+     .setColor("RED")
+     .setThumbnail(message.author.avatarURL)
+     .setDescription("You Are Kicked")
+     .addField("Kicked By", message.author.username,false)
+     .addField("Reason:", kickreason,false)
+     .setFooter("GOLD BOT", client.user.avatarURL)
+     
+       kickeduser.send(DM);
+
+
+
+
+    }
+    });
+    
+    
+    
+    
+    
+    
+client.on('message', message =>  {
+    if(message.content.startsWith("-ban")) {
+
+                  
+    
+        let banneduser = message.mentions.users.first();
+        if(!message.channel.guild) return
+        if(!message.member.hasPermission("BAN_MEMBERS")) return message.replay("You Don't Have Permission 'BAN_MEMBERS' ليس لديك صلاحية ")
+        if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.replay("i Don't Have Permission 'BAN_MEMBERS' ليس لدي صلاحية ")
+        if(!banneduser) return message.reply("**I Can't Find This User | لم اجد هاد الشخص**")
+        if(!message.guild.member(banneduser).bannable) return message.replay("I Can't Ban This Person | لا يمكنني تبنيد هاد الشخص");
+        let banreason = message.content.split(" ").slice(2).join();
+       if(!banreason) return message.reply("Please Type Reason... | رجاءا ضع سبب")
+       
+
+
+       var DK= new Discord.RichEmbed()
+       .setFooter("GOLD BOT", client.user.avatarURL)
+       .setColor('GOLD')
+       .setThumbnail(message.author.avatarURL)
+       .setTitle("BANNED !")
+       .addField("Banned User:", banneduser)
+       .addField("Banned By:",  message.author.username)
+       .addField("Reason:", banreason)
+    
+    
+       message.channel.send(DK);
+    
+    
+           message.guild.member(banneduser).ban(banreason);
+    
+    
+    
+         let logroom = message.guild.channels.find("name", "log")
+         if(!logroom) return;
+    
+         var log= new Discord.RichEmbed() 
          
-  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
-  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
-  let user = message.mentions.users.first();
-  let reason = message.content.split(" ").slice(2).join(" ");
-  /*let b5bzlog = client.channels.find("name", "5bz-log");
-  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
-  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
-  if(!reason) return message.reply ("**اكتب سبب الطرد**");
-  if (!message.guild.member(user)
-  .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
-
-  message.guild.member(user).ban(7, user);
-
-  const banembed = new Discord.RichEmbed()
-  .setAuthor(`BANNED!`, user.displayAvatarURL)
-  .setColor("RANDOM")
-  .setTimestamp()
-  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
-  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
-  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
-  message.channel.send({
-    embed : banembed
-  })
-}
-});
-
-client.on('message', message => {
-	var prefix = "-"
-  if (message.author.x5bz) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "kick") {
-               if(!message.channel.guild) return message.reply('** This command only for servers**');
+         .setColor("RED")
+         .setThumbnail(message.author.avatarURL)
+         .addField("Banned Member Name", banneduser,false)
+         .addField("Banned By", message.author.username,false)
+         .addField("Banned In:", message.channel,false)
+         .addField("Reason:", banreason,false)
+         .setFooter("GOLD BOT", client.user.avatarURL)
          
-  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**You Don't Have ` KICK_MEMBERS ` Permission**");
-  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("**I Don't Have ` KICK_MEMBERS ` Permission**");
-  let user = message.mentions.users.first();
-  let reason = message.content.split(" ").slice(2).join(" ");
-  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
-  if(!reason) return message.reply ("**اكتب سبب الطرد**");
-  if (!message.guild.member(user)
-  .kickable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
-
-  message.guild.member(user).kick();
-
-  const kickembed = new Discord.RichEmbed()
-  .setAuthor(`KICKED!`, user.displayAvatarURL)
-  .setColor("RANDOM")
-  .setTimestamp()
-  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
-  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
-  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
-  message.channel.send({
-    embed : kickembed
-  })
-}
-});
+           logroom.send(log);
+    
+    
+           var DM= new Discord.RichEmbed() 
+         
+         .setColor("RED")
+         .setThumbnail(message.author.avatarURL)
+         .setDescription("You Are Banned")
+         .addField("Banned By", message.author.username,false)
+         .addField("Reason:", banreason,false)
+         .setFooter("GOLD BOT", client.user.avatarURL)
+         
+           banneduser.send(DM);
+    
+    
+    
+    
+        }
+        });
 
 
 
